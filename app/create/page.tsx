1cb2b1402ca1cf83c72b548afb13ba666c3ded8b
Maia <matteusmaia889@gmail.com>
2024-07-14 23:31:43 -0300

    feat: Create page

---
+++ app/create/page.tsx
@@ -0,0 +1,25 @@
+export default function CreatePage(){
+  return(
+    <div>
+      <h2 className="text-2xl font-bold my-8">Add New Interpretation</h2>
+
+      <form action="" className="flex gap-3 flex-col">
+        <input 
+          type="text"
+          name="term"
+          placeholder="Term"
+          className="py-1 px-4 border rounded-md"
+        />
+
+        <textarea 
+          name="interpretation"
+          placeholder="Interpretation"
+          rows={4}
+          className="py-1 px-4 border rounded-md resize-none"
+        />
+
+        <button className="bg-black text-white mt-5 px-4 py-1 rounded-md cursor-pointer">Add Interpretation</button>
+      </form>
+    </div>
+  )
+}

d129f78ac9befbb8b69f3ec3b5973b2ed8851799
Maia <matteusmaia889@gmail.com>
2024-07-14 23:32:00 -0300

    feat: header on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div  className="max-3-3xl mx-auto text-slate-800">
-          <header className="p-6 border-b flex justify-between bg-blue-500 rounded-bl-lg rounded-br-lg ">
-            <Link href="/">Tech Interpretation</Link>
-            <Link href="/create">Add New</Link>
+        <div  className="max-w-3xl mx-auto text-slate-800">
+          <header className="p-6 border-b flex items-center justify-between bg-blue-500 rounded-bl-lg rounded-br-lg ">
+            <Link className="text-2xl font-bold text-white" href="/">Tech Interpretation</Link>
+            <Link className="bg-slate-100 grid place-items-center py-2 px-4 rounded-full font-bold shadow-md" href="/create">Add New</Link>
           </header>
-          <main>
+          <main className="p-5 text-lg">
             {children}
           </main>
         </div>
